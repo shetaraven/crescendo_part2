844b1883fc0b61030d209c89d74e7146a35c01b9
Jomar Yaco Adalan <jya@Jomars-MacBook-Pro.local>
2021-02-19 15:52:02 +0800

    fixes

--- assets/js/acme.js
+++ assets/js/acme.js
@@ -1,5 +1,5 @@
 function toggle_menu_func() {
-    let element = document.getElementById("toggle_menu");
-    element.classList.toggle("navbar_mobile--checked");
-    document.getElementById("dimmer").classList.toggle("dimmer--show");
+    let element = document.getElementsByClassName("mobile_toggle_menu");
+    element[0].classList.toggle("navbar_mobile--checked");
+    document.getElementsByClassName("dimmer")[0].classList.toggle("dimmer--show");
 }
